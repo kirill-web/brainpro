e6e8775c3887feb55798db89992eace6d421d3d0
Kirill <kirillpwnography@gmail.com>
2017-08-21 01:10:07 +0300

    pages in process

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	
+	// Мобильное меню
 	$('.menu-toggle, .menu-toggle--inside').on('click', function(){
 		$('body').addClass('no-scroll');
 		$('.main-menu').show(300);
@@ -10,4 +11,26 @@ $(document).ready(function(){
 		$('body').removeClass('no-scroll');
 	});
 	
+	// Услуги на мобильных на главной
+	$('.service-sec__mobile-item').on('click', function(){
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).parent('.container').removeClass('active');
+			$(this).next('.service-sec__mobile-hide').slideUp(200);
+		} else {
+			$(this).addClass('active');
+			$(this).parent('.container').addClass('active');
+			$(this).next('.service-sec__mobile-hide').slideDown(200);
+		}
+	});
+	
+	// Табы с ценами на странице принт
+	$('.prices-sec__tab').on('click', function(){
+		$('.prices-sec__tab').removeClass('active');
+		$(this).addClass('active');
+		var dataId = $(this).attr('data-id');
+		
+		$('.prices-sec__items').removeClass('active');
+		$('.prices-sec__items#' + dataId).addClass('active');
+	});
 });
